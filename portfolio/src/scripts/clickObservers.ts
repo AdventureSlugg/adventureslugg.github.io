9702f27262ab24e417e6e1467bc396a723ca55a0
Zoe <zmb6893@g.rit.edu>
2025-12-01 10:14:09 -0800

    Implemented the top bar functionality for exit, minimize, and hide

---
+++ portfolio/src/scripts/clickObservers.ts
@@ -0,0 +1,63 @@
+const openLink = (url: string, confirmation: boolean = true) => {
+	const link = document.createElement('a');
+	link.href = url;
+	link.target = "_blank"
+
+	if (!confirmation) {
+		link.click();
+	} else if (confirm(`You are being redirected to ${url}. Would you like to proceed?`)) {
+		link.click();
+	} 
+
+	document.removeChild(link);
+
+}
+
+const actionMap: Record<string, CallableFunction> = {
+	'explorerClickable': () => {
+		// Expand/Minimize the Explorer side-bar.
+		const explorerElement = document.getElementById('explorer-section') as HTMLElement;
+		explorerElement.style.display = explorerElement.style.display === 'none' ? 'block' : 'none'
+	},
+	'searchClickable': () => {
+		// TODO: Open up the Search function.
+		console.log("Not yet implemented...");
+	},
+	'devpostClickable': () => {
+		openLink('https://devpost.com/zmb6893')
+	},
+	'githubClickable': () => {
+		openLink('https://github.com/AdventureSlugg')
+	},
+	'linkedinClickable': () => {
+		openLink('https://www.linkedin.com/in/zoe-bingham/')
+	},
+	'exit-option': () => {
+		alert('Oh no you don\'t! Get back there!');
+	},
+	'minimize-option': () => {
+		document.getElementsByClassName('upside-down').length > 0 ?
+		document.getElementById('base')!.classList.replace('upside-down', 'right-side-up') :
+		document.getElementById('base')!.classList.replace('right-side-up', 'upside-down');
+	},
+	'hide-option': () => {
+		openLink('https://www.youtube.com/watch?v=E4WlUXrJgy4', false);
+	}
+
+}
+
+const clickables = document.querySelectorAll('div.clickable');
+const buttonClickables = document.querySelectorAll('button.clickable');
+
+clickables.forEach(clickable => {
+	clickable.addEventListener('click', () => {
+		// TODO: Highlight the clickable option and remove highlight from all other clickables
+		actionMap[clickable.id]();
+	})
+})
+
+buttonClickables.forEach(clickable => {
+	clickable.addEventListener('click', () => {
+		actionMap[clickable.id]();
+	})
+})
